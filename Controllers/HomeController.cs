a9bbca6ca9fabb16c15dbf14a4db6d291bb6b313
AtilaEzequiel <112121260+AtilaEzequiel@users.noreply.github.com>
2023-06-07 20:43:50 -0300

    render

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcMovie.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MvcMovie.Models;
+using NuGet.Protocol.Plugins;
+using System.Diagnostics;
+using System.Drawing;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Microsoft.Data.SqlClient;
 
 namespace MvcMovie.Controllers
 {
@@ -12,12 +21,55 @@ namespace MvcMovie.Controllers
         {
             _logger = logger;
         }
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MovieADO;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
+        List<Carrera> list = new List<Carrera>();
+       
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "Select * from Carrera Where Id=1";
+                //string queryString = "INSERT INTO MovieADO (Id, titulo, fecha, genero, precio) VALUES (10, 'Delta', 15/12/1999, 'magia', 600);";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //  command.ExecuteReader(queryString);
+                //command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Carrera movieADOs = new Carrera()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Name = reader[1].ToString(),
+                       //
+                       //ReleaseDate = DateTime.Parse(reader[2].ToString()),
+                        Description = reader[2].ToString(),
+                      //  Price = int.Parse(reader[4].ToString()),
+                    };
+                    return View(movieADOs);
+
+                    //  list.Add(movieADOs);
+                    //list.Add(movieADOs1);
+                }
+
+
+
+                connection.Close();
+                return View();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
+
+        
         public IActionResult Privacy()
         {
             return View();
---
+++ Models/Carrera.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcMovie.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
